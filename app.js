07871c700a8693e575f4f53ffb78db2a063fd4d2
ivopc <ivoopc@gmail.com>
2020-03-15 20:15:23 -0300

    big update

---
+++ app.js
@@ -0,0 +1,15 @@
+const 
+    express = require("express"),
+    app = express(),
+    http = require("http").createServer(app),
+    io = require("socket.io")(http);
+
+const Core = require("./server/core");
+
+app.use(express.static("./public"));
+
+io.on("connection", new Core().conn);
+
+http.listen(3000, () => {
+    console.log("Server listening on :3000");
+});
